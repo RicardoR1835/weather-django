5e44252f51e3baa46d3ef8c61a39a2284650da35
Ricardo Rivera <rick.rivera1835@gmail.com>
2022-06-28 08:43:05 -0500

    keys

--- weather_app/settings.py
+++ weather_app/settings.py
@@ -26,7 +26,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-%_%x3za6d6m1hn9czq#9r6#c4+djhvaz$4qg-wbuijf!p5w3yr'
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 API_KEY = str(os.getenv('API_KEY'))
 
